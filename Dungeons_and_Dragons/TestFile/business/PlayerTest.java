57a3ec4feb3c0103df57c3366bb90557ffc9e571
Nimrod <klemer@post.bgu.ac.il>
2022-07-22 23:14:27 +0300

    cleaning PlayerTest TO DO

--- Dungeons_and_Dragons/TestFile/business/PlayerTest.java
+++ Dungeons_and_Dragons/TestFile/business/PlayerTest.java
@@ -29,31 +29,11 @@ class PlayerTest {
     }
 
     @org.junit.jupiter.api.Test
-    void getPlayerLevel() {
-    }
-
-    @org.junit.jupiter.api.Test
-    void addXP() {
-    }
-
-    @org.junit.jupiter.api.Test
-    void setExperience() {
-    }
-
-    @org.junit.jupiter.api.Test
-    void getExperience() {
-    }
-
-    @org.junit.jupiter.api.Test
     void battle() {
     }
 
     @org.junit.jupiter.api.Test
     void maxAttackBattle() {
-    }
-
-    @org.junit.jupiter.api.Test
-    void getIsAlive() {
     }
 
     @org.junit.jupiter.api.Test
